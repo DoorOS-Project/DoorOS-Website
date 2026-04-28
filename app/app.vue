6e5e71984c86ef2cfa19d6b16a3ec3cbee6afa85
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2026-04-28 20:53:37 +0200

    fix: icon not working because not being uploaded to the URL's root

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ useHead({
 		{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
 	],
 	link: [
-		{ rel: 'icon', href: '/favicon.ico' }
+		{ rel: 'icon', href: 'https://github.com/DoorOS-Project.png' }
 	],
 	htmlAttrs: {
 		lang: 'en'
